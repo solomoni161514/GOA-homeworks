c341941ee2a37169201da7e3e38558fbe32fb7d9
solomoni161514 <solomonibujiashvili0@gmail.com>
2024-09-28 18:55:00 +0400

    first commit

---
+++ level02/homework/lesson2.py
@@ -0,0 +1,13 @@
+print("თამაშები იწვევს დამოკიდებულებას გვართმევს დიდ დროს აღარ გვრჩება დრო სწავლისთვის")
+
+
+
+print("sandro abashidze")
+print("nodar chokoraia")
+print("luka wulaia")
+print("temo qveladze")
+print("giorgi mamukashvili")
+print("giorgi balamwarashvili")
+print("gagi xalvashi")
+print("nika tuzuridze")
+print("andria janjulia")
