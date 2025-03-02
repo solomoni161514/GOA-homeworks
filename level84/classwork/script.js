7fa5d64f42965ba8d7aee64badca5de8707240e1
solomoni161514 <solomonibujiashvili0@gmail.com>
2025-03-02 13:39:45 +0400

    GOA BEST!

---
+++ level84/classwork/script.js
@@ -0,0 +1,42 @@
+// function main(name, lName, age, birthYear) {
+//     return {name, lName, age, birthYear};
+// }
+
+// let name1 = prompt("Enter your name:");
+// let lName1 = prompt("Enter your last name:");
+// let age1 = parseInt(prompt("Enter your age:"));
+// let birthYear1 = parseInt(prompt("Enter your birth year:"));
+
+// if (name1 === name) {
+//     console.log("name1");
+//     console.log("corect");
+// }
+
+// else if (lName1 === lName) {
+//     console.log("lName1");
+//     console.log("corect");
+// }
+
+// else if (age1 === age) {
+//     console.log("age1");
+//     console.log("corect");
+// }
+
+// else if (birthYear1 === birthYear) {
+//     console.log("birthYear1");
+//     console.log("corect");
+// }
+
+// else {
+//     console.log("incorect");
+// }
+
+
+// main("solomoni", "bujiashvili", "12", "2012");
+
+
+function area(width, length) {
+    return width * length;
+}
+
+console.log(area(prompt("Enter widgh:"), prompt("Enter light:")));
